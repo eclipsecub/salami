621698f2e175257cdb4807fa0e92ec8be5c90fff
EclipseCub <lolmaster45@hotmail.com>
2020-02-05 17:32:43 -0500

    our initial database

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>GIT TEST</title>
+</head>
+<body>
+    <header>
+        <h1>GIT TEST</h1>
+    </header>
+    
+    <a href="./table.php">Table of Countries</a>
+</body>
+</html>
